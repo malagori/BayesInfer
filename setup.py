e3a78ab268f1fc289170882c2508324147ffa43c
Mehmood Khan <malagori@kth.se>
2013-04-18 12:13:54 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+setup(
+    name='malagori',
+    version='0.1dev',
+    author='Mehmood Alam Khan',
+    author_email='malagori@kth.se',
+    url='https://github.com/malagori/BayesInfer',
+    packages=['bayesInfer',],
+    scripts = ['scripts/main.py'],
+    license='GPLv3',
+    long_description=open('README.md').read(),
+)
