eb1f93dc5318edae3ca51415dde56b3687667df8
Mehmood Khan <malagori@kth.se>
2013-05-06 16:39:21 +0200

    update setup.py file

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author_email='malagori@kth.se',
     url='https://github.com/malagori/BayesInfer',
     packages=['bayesInfer',],
-    scripts = ['scripts/main.py'],
+    scripts = ['scripts/calculateBDeuScore.py'],
     license='GPLv3',
     long_description=open('README.md').read(),
 )
