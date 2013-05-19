bd155b97d71e886ac8b414a5a4c074c495a6adb9
Mehmood Khan <malagori@kth.se>
2013-05-19 10:17:08 +0200

    adding data and structure file in examples

--- bayesInfer/readGraph.py
+++ bayesInfer/readGraph.py
@@ -5,7 +5,8 @@ __license__ = "GPL"
 __credits__ = ["Mehmood Alam Khan", "Pekka Parviainen"]
 
 import sys
-
+import itertools
+from random import randrange
 from bayesInfer.node import Node
 
 def readInitialStructure(infile):
@@ -14,7 +15,7 @@ def readInitialStructure(infile):
     allNodesObj={}
     for line in open(infile, 'r'):
         var, cardinality, pa=line.split(':')
-        if len(pa) != 0:
+        if len(pa) > 0:
             parents=pa.split(',')
         node=Node()
         node.setR(cardinality)
@@ -24,3 +25,24 @@ def readInitialStructure(infile):
         node.setParents(parents)
         allNodesObj[var]=node
     return allNodesObj
+
+def generateData():
+    """ generate dataset """
+    wf= open('data1.txt', 'w')
+    alphabits=['A','B','C','D', 'Counts']
+    cardinality= [2,2,2,2]
+    
+    for i in xrange(0,4):
+        wf.write(str(alphabits[i]+',')) # print except last column name
+    wf.write(alphabits[4]) # print the last column name
+    wf.write('\n')
+    values=[[0, 1], [0, 1], [0, 1], [0, 1]]
+    for i in itertools.product(*values):
+        for j in xrange(0,len(i)):
+            wf.write(str(i[j])+',')
+        #wf.write(str(i[-1]))
+        wf.write(str(randrange(100)))
+        wf.write('\n')
+    wf.close() 
+        
+    
