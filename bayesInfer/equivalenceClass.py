d089e854957a8da97c83b4e410be3cbafa9cafe4
Mehmood Khan <malagori@kth.se>
2013-09-16 16:43:26 +0200

    fixing import

--- bayesInfer/equivalenceClass.py
+++ bayesInfer/equivalenceClass.py
@@ -8,13 +8,13 @@ import os
 import numpy as np
 from mlabwrap import mlab
 from node import Node
-#from bayesInfer import matlab_lib
+import matlab_lib
 from beneWrapper import BeneWrapper
 
 class EquivalenceClass(object):
     def __init__(self):
-        self.matlabLibPath='/gulo/glob/mehmood/projects/bayesian/BayesInfer/bayesInfer/matlab_lib'
-        #self.matlabLibPath= os.path.dirname(os.path.abspath(matlab_lib.__file__))
+        #self.matlabLibPath='/gulo/glob/mehmood/projects/bayesian/BayesInfer/bayesInfer/matlab_lib'
+        self.matlabLibPath= os.path.dirname(os.path.abspath(matlab_lib.__file__))
         mlab.addpath(self.matlabLibPath)
         
     def getOptDag(self, vdFile, dataFile, score, outDirectory, totalVaiables, cardinality):
