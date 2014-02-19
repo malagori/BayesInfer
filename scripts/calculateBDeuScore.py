91d2c1a8c14237c0a35c140f6aa830a2c4b85296
Mehmood Khan <malagori@kth.se>
2014-02-19 19:50:05 +0100

    fixing

--- scripts/calculateBDeuScore.py
+++ scripts/calculateBDeuScore.py
@@ -422,7 +422,7 @@ def probAcceptance( e, enew, T):
         #print "e : %f  enew: %f" % (e, enew)
     return prob        
 
-def simulatedAnealing( allNodeObjects, hiddenVar, previousScore, sIndex, iterations, outFile, decrementValue, alpha ):
+def simulatedAnealing( df, allNodeObjects, hiddenVar, previousScore, sIndex, iterations, outFile, decrementValue, alpha ):
     """
         This function implements the simulated Anealing algorithm (wiki) 
     """
@@ -436,7 +436,10 @@ def simulatedAnealing( allNodeObjects, hiddenVar, previousScore, sIndex, iterati
     objCBDeuOldState = allNodeObjects
     j               = sIndex
     bestDf          = pd.DataFrame(index=None, columns=None)
+    
     dfCurrent       = df.copy()
+    print "dfCurrent"
+    print dfCurrent
 #    bestDf          = df.copy()
 #    dfCurrent       = df.copy()
     with open(outFile, 'w') as wf:
@@ -450,9 +453,9 @@ def simulatedAnealing( allNodeObjects, hiddenVar, previousScore, sIndex, iterati
                 flag = False
             else:
                 flag = True
-
-            countPerturbation(hiddenVar, j, decrementValue, incrementFlag=flag)     
             
+            countPerturbation(hiddenVar, j, decrementValue, incrementFlag=flag)     
+            print df
             j=rNumber.randint(0, df.shape[0]-1) # randomly select another record for next iteration
             
             nodesBDeuScore= []
@@ -473,8 +476,8 @@ def simulatedAnealing( allNodeObjects, hiddenVar, previousScore, sIndex, iterati
             
             if acceptprob < rnum:# reject the current state 
                 allNodeObjects= objCBDeuOldState          # go back to the old state
-                global df
-                df = dfCurrent.copy()
+                #global df
+                #df = dfCurrent.copy()
                 wf.write("Rejected: Best bdeuscore: %f, Current bdeuscore: %f, proposal bdeuscore: %f, coin: %d , temp: %f, prob: %f rNumber: %f\n" % (ebest, e, enew, num, T, acceptprob, rnum))
             else:  # accept the new state
                 objCBDeuOldState= allNodeObjects
@@ -640,7 +643,7 @@ def main(argv):
         print "Simulated Anealing starts now"
         sIndex                  = rNumber.randint(0,df.shape[0]-2)
         rs.storeSate(stateOutFile)
-        simulatedAnealing( allNodeObjects, h, totalPreviousBDeuScore, sIndex, maxIter, outputFile+".sim", decrementValue, alpha )
+        simulatedAnealing( df, allNodeObjects, h, totalPreviousBDeuScore, sIndex, maxIter, outputFile+".sim", decrementValue, alpha )
         
     elif steepestAsent == True:
         iterations=0
